e72fc11f3927206863da30af4d73b85bcc3e0ea2
Tamas Molnar <tamas.molnar@liferay.com>
2021-04-13 12:04:52 +0200

    Refactor

--- src/currencyConverter.js
+++ src/currencyConverter.js
@@ -1,21 +1,36 @@
-function create(fromCurrency, toCurrency, exchangeRate, id, parent) {
+function create(fromCurrency, toCurrency, exchangeRate, parent) {
   let divWrapper = document.createElement("div");
+
   parent.appendChild(divWrapper);
 
-  let header = document.createElement("p");
-  header.innerHTML = fromCurrency + " ---> " + toCurrency;
+  let header = document.createElement("span");
+  header.innerHTML = fromCurrency + " > " + toCurrency + "<br />";
 
   divWrapper.appendChild(header);
 
+  divWrapper.appendChild(document.createElement("p"));
+
   let fromInput = document.createElement("input");
-  fromInput.id = fromCurrency;
+
+  fromInput.setAttribute("type", "number");
+  fromInput.setAttribute("value", 0);
 
   let toInput = document.createElement("input");
-  toInput.id = toCurrency;
   toInput.disabled = true;
 
   header.appendChild(fromInput);
+
+  let fromLabel = document.createElement("label");
+  fromLabel.innerHTML = fromCurrency;
+  fromLabel.setAttribute("for", fromCurrency);
+  header.appendChild(fromLabel);
+
   header.appendChild(toInput);
+
+  let toLabel = document.createElement("label");
+  toLabel.innerHTML = toCurrency;
+  toLabel.setAttribute("for", toCurrency);
+  header.appendChild(toLabel);
 
   fromInput.addEventListener("input", (e) => {
     toInput.value = e.target.value * exchangeRate;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import { create } from "./currencyConverter.js";
 
-create("USD", "BTC", 500000, "", document.body);
+create("USD", "BTC", 500000, document.body);
 
-create("EUR", "HUF", 358, "", document.body);
+create("EUR", "HUF", 360, document.body);
