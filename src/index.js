4958b11767af459ef5f654a1aa4fbdecb520ff06
Tamas Molnar <tamas.molnar@liferay.com>
2021-04-23 14:34:10 +0200

    Theme

--- src/index.js
+++ src/index.js
@@ -9,3 +9,30 @@ EUR_HUF.create();
 
 let USD_HUF = new CurrencyConverter("USD", "HUF", 300, document.body);
 EUR_HUF.create();
+
+let goDark = function () {
+  document.body.classList.add("dark");
+};
+
+let goLight = function () {
+  document.body.classList.remove("dark");
+};
+
+let themeSelector = document.createElement("select");
+
+document.body.appendChild(themeSelector);
+
+let lightOption = document.createElement("option");
+lightOption.innerHTML = "Light";
+lightOption.setAttribute("value", "light");
+
+let darkOption = document.createElement("option");
+darkOption.innerHTML = "Dark";
+darkOption.setAttribute("value", "dark");
+
+themeSelector.appendChild(lightOption);
+themeSelector.appendChild(darkOption);
+
+themeSelector.addEventListener("change", (e) => {
+  e.target.value === "light" ? goLight() : goDark();
+});
