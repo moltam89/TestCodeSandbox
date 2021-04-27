3a8a3ed75f40a5fc2c86eead6db9e946150494e0
Tamas Molnar <tamas.molnar@liferay.com>
2021-04-27 10:50:00 +0200

    Fix currencies

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ let app = document.getElementById("app");
 let themeSelector = new ThemeSelector(app);
 themeSelector.create();
 
-let USD_BTC = new CurrencyConverter("USD", "HUF", 300, app);
+let USD_BTC = new CurrencyConverter("USD", "BTC", 50000, app);
 USD_BTC.create();
 
 let EUR_HUF = new CurrencyConverter("EUR", "HUF", 360, app);
 EUR_HUF.create();
 
 let USD_HUF = new CurrencyConverter("USD", "HUF", 300, app);
-EUR_HUF.create();
+USD_HUF.create();
