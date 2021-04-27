f4dad832846639cd3c0c2c21debd5fa21070c3d8
Tamas Molnar <tamas.molnar@liferay.com>
2021-04-27 10:11:36 +0200

    Append elements to app instead of body

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import "./styles.css";
 import CurrencyConverter from "./currencyConverter.js";
 
-let USD_BTC = new CurrencyConverter("USD", "HUF", 300, document.body);
+let app = document.getElementById("app");
+
+let USD_BTC = new CurrencyConverter("USD", "HUF", 300, app);
 USD_BTC.create();
 
-let EUR_HUF = new CurrencyConverter("EUR", "HUF", 360, document.body);
+let EUR_HUF = new CurrencyConverter("EUR", "HUF", 360, app);
 EUR_HUF.create();
 
-let USD_HUF = new CurrencyConverter("USD", "HUF", 300, document.body);
+let USD_HUF = new CurrencyConverter("USD", "HUF", 300, app);
 EUR_HUF.create();
 
 let goDark = function () {
